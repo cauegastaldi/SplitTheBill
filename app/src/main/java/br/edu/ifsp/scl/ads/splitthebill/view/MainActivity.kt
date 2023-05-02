de11a1eee5bb0e45f3c0cf64e7d908e25e3bade9
Caue <cauegastaldi@outlook.com>
2023-05-02 20:36:03 -0300

    feat: added adapter in MainActivity

--- app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/view/MainActivity.kt
@@ -3,10 +3,27 @@ package br.edu.ifsp.scl.ads.splitthebill.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.edu.ifsp.scl.ads.splitthebill.R
+import br.edu.ifsp.scl.ads.splitthebill.adapter.MemberAdapter
+import br.edu.ifsp.scl.ads.splitthebill.databinding.ActivityMainBinding
+import br.edu.ifsp.scl.ads.splitthebill.model.Member
 
 class MainActivity : AppCompatActivity() {
+
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val memberList: MutableList<Member> = mutableListOf()
+
+    private val memberAdapter: MemberAdapter by lazy {
+        MemberAdapter(this, memberList)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
+        supportActionBar?.subtitle = "Lista de Integrantes"
+
+        amb.membersLv.adapter = memberAdapter
     }
 }
