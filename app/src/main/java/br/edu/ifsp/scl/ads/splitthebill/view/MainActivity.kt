91bb853981d82aa211da61c6cd0e9136fc4514b6
Caue <cauegastaldi@outlook.com>
2023-05-02 21:40:05 -0300

    feat: created xml file for main activity's menu and inflated layout of the menu on main activity's layout

--- app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/view/MainActivity.kt
@@ -2,6 +2,7 @@ package br.edu.ifsp.scl.ads.splitthebill.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import br.edu.ifsp.scl.ads.splitthebill.R
 import br.edu.ifsp.scl.ads.splitthebill.adapter.MemberAdapter
 import br.edu.ifsp.scl.ads.splitthebill.databinding.ActivityMainBinding
@@ -25,5 +26,10 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.subtitle = "Lista de Integrantes"
 
         amb.membersLv.adapter = memberAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
